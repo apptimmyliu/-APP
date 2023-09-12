124f708a1b2002dcd8c5d959e2f42088a2cee206
Timmy <timmyliu1997@hotmail.com>
2023-09-12 18:03:51 +0800

    Initial Commit

---
+++ 小費，匯率，單位換算/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  小費，匯率，單位換算
+//
+//  Created by Timmy on 2023/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
